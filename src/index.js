3059ac7aa4588067507e5a41315f63c4955efb61
Berezhnyi <kivin13@gmail.com>
2019-06-22 15:43:45 +0300

    final

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-function isLegalRectangle (rectangle) {
+function isNotColapsed (rectangle) {
 
 	if (rectangle.height <= 0 || rectangle.width <= 0) {
 		return false;
@@ -8,46 +8,48 @@ function isLegalRectangle (rectangle) {
 }
 
 export function areIntersected (firstRectangle, secondRectangle) {
-	let ay = firstRectangle.top;
-	let ax = firstRectangle.left;
-	let ay2 = firstRectangle.height + firstRectangle.top;
-	let ax2 = firstRectangle.width + firstRectangle.left;
-	let by = secondRectangle.top;
-	let bx = secondRectangle.left;
-	let by2 = secondRectangle.height + secondRectangle.top;
-	let bx2 = secondRectangle.width + secondRectangle.left;
+	let firstOrdinateBeginning = firstRectangle.top;
+	let fisrtAbscissaBeginning = firstRectangle.left;
+	let firstOrdinateEnd = firstRectangle.height + firstRectangle.top;
+	let fisrtAbscissaEnd = firstRectangle.width + firstRectangle.left;
+	let secondOrdinateBeginning = secondRectangle.top;
+	let secondAbscissaBeginning = secondRectangle.left;
+	let secondOrdinateEnd = secondRectangle.height + secondRectangle.top;
+	let secondAbscissaEnd = secondRectangle.width + secondRectangle.left;
 
-	if (((ax <= bx && bx < ax2) || (bx <= ax && ax < bx2)) && ((ay <= by && by < ay2) || (by <= ay && ay < by2))) {
+	if (((fisrtAbscissaBeginning <= secondAbscissaBeginning && secondAbscissaBeginning < fisrtAbscissaEnd)
+	|| (secondAbscissaBeginning <= fisrtAbscissaBeginning && fisrtAbscissaBeginning < secondAbscissaEnd))
+	&& ((firstOrdinateBeginning <= secondOrdinateBeginning && secondOrdinateBeginning < firstOrdinateEnd)
+	|| (secondOrdinateBeginning <= firstOrdinateBeginning && firstOrdinateBeginning < secondOrdinateEnd))) {
 		return true;
 	}
-
-
 	return false;
 }
 
 function areIntersectsParental (firstRectangle, secondRectangle) {
-	let ay = firstRectangle.top;
-	let ax = firstRectangle.left;
-	let ay2 = firstRectangle.height + firstRectangle.top;
-	let ax2 = firstRectangle.width + firstRectangle.left;
-	let by = secondRectangle.top;
-	let bx = secondRectangle.left;
-	let by2 = secondRectangle.height + secondRectangle.top;
-	let bx2 = secondRectangle.width + secondRectangle.left;
+	let firstOrdinateBeginning = firstRectangle.top;
+	let fisrtAbscissaBeginning = firstRectangle.left;
+	let firstOrdinateEnd = firstRectangle.height + firstRectangle.top;
+	let fisrtAbscissaEnd = firstRectangle.width + firstRectangle.left;
+	let secondOrdinateBeginning = secondRectangle.top;
+	let secondAbscissaBeginning = secondRectangle.left;
 
-	if (((ax <= bx && bx < ax2)) && ((ay <= by && by < ay2))) {
+	if ((fisrtAbscissaBeginning <= secondAbscissaBeginning && secondAbscissaBeginning < fisrtAbscissaEnd)
+	&& (firstOrdinateBeginning <= secondOrdinateBeginning && secondOrdinateBeginning < firstOrdinateEnd)) {
 		return true;
 	}
+
 	return false;
 }
 
 export function filterVisible (maternalRectangle, rectangleArray) {
 	let resultRectangleArray = [];
 
-	if (isLegalRectangle(maternalRectangle)) {
+	if (isNotColapsed(maternalRectangle)) {
 		resultRectangleArray = rectangleArray
-		.filter(rectangle => (areIntersectsParental(maternalRectangle, rectangle))
-		&& isLegalRectangle(rectangle));
+			.filter(rectangle =>
+				(areIntersectsParental(maternalRectangle, rectangle))
+				&& isNotColapsed(rectangle));
 
 		return resultRectangleArray;
 	}
